42abd3a9f3a5f1025d28d4a8089ab533ec37dca8
Alexander Matheson <94473549+AlexanderMatheson@users.noreply.github.com>
2026-09-22 18:08:15 -0300

    Update EdgeWeightContraction.java

--- Code/NodeContractions/EdgeWeightContraction.java
+++ Code/NodeContractions/EdgeWeightContraction.java
@@ -2,6 +2,6 @@ package NodeContractions;
 
 import java.util.ArrayList;
 
-public abstract class EdgeWeightContracti9n {
+public abstract class EdgeWeightContraction {
 	abstract public GraphRepresentations contract(int[][] graphMatrix, ArrayList<ArrayList<Integer>> adjList, ArrayList<ArrayList<Integer>> paths);
 }
